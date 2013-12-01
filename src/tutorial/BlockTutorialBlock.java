ef31d03beab9bd1f51f89d5a44df2c383c6daf77
clarkyzeki <clarkyzeki@tpg.com.au>
2013-12-01 19:24:20 +1000

    Applied custom textures.

--- src/tutorial/BlockTutorialBlock.java
+++ src/tutorial/BlockTutorialBlock.java
@@ -2,7 +2,11 @@ package tutorial;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.Icon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockTutorialBlock extends Block
 {
@@ -12,5 +16,32 @@ public class BlockTutorialBlock extends Block
 		super(id, material);
 		this.setCreativeTab(CreativeTabs.tabBlock);
 	}
-
+	
+	@SideOnly(Side.CLIENT)
+	public static Icon topIcon;
+	@SideOnly(Side.CLIENT)
+	public static Icon bottomIcon;
+	@SideOnly(Side.CLIENT)
+	public static Icon sideIcon;
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister icon)
+	{
+		topIcon = icon.registerIcon(Tutorial.modid + ":" + (this.getUnlocalizedName().substring(5)) + "_top");
+		bottomIcon = icon.registerIcon(Tutorial.modid + ":" + (this.getUnlocalizedName().substring(5)) + "_bottom");
+		sideIcon = icon.registerIcon(Tutorial.modid + ":" + (this.getUnlocalizedName().substring(5)) + "_side");
+		
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int metadata) {
+	if(side == 0) {
+	return bottomIcon;
+	} else if(side == 1) {
+	return topIcon;
+	} else {
+	return sideIcon;
+	}
+	}
 }
--- src/tutorial/ItemTutorial.java
+++ src/tutorial/ItemTutorial.java
@@ -1,5 +1,8 @@
 package tutorial;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
@@ -9,6 +12,13 @@ public class ItemTutorial extends Item
 	public ItemTutorial(int id) {
 		super(id);
 		this.setCreativeTab(CreativeTabs.tabMaterials);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister)
+	{
+		this.itemIcon = par1IconRegister.registerIcon(Tutorial.modid + ":" + (this.getUnlocalizedName().substring(5)));
+		
 	}
 	
 }
